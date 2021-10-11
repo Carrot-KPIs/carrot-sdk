c2f98ea27fbd4500f4cc8b4ece96b4563ff96ab4
Federico Luzzi <fedeluzzi00@gmail.com>
2021-10-11 17:46:22 +0200

    feat: add get native currency function to token entity

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -6,6 +6,32 @@ export class Token extends Currency {
   public readonly chainId: ChainId
   public readonly address: string
 
+  public static readonly WETH: { [key: number]: Token } = {
+    [ChainId.RINKEBY]: new Token(
+      ChainId.RINKEBY,
+      '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+      18,
+      'WETH',
+      'Wrapped Ether'
+    ),
+    [ChainId.XDAI]: new Token(
+      ChainId.XDAI,
+      '0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1',
+      18,
+      'WETH',
+      'Wrapped Ether on xDai'
+    ),
+  }
+
+  public static readonly WXDAI: { [key: number]: Token } = {
+    [ChainId.XDAI]: new Token(ChainId.XDAI, '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d', 18, 'WXDAI', 'Wrapped xDAI'),
+  }
+
+  private static readonly NATIVE_CURRENCY_WRAPPER: { [chainId in ChainId]: Token } = {
+    [ChainId.RINKEBY]: Token.WETH[ChainId.RINKEBY],
+    [ChainId.XDAI]: Token.WXDAI[ChainId.XDAI],
+  }
+
   public constructor(chainId: ChainId, address: string, decimals: number, symbol: string, name: string) {
     super(symbol, name, decimals)
     this.chainId = chainId
@@ -14,6 +40,10 @@ export class Token extends Currency {
 
   public equals(other: Token): boolean {
     return this === other || (this.chainId === other.chainId && this.address === other.address)
+  }
+
+  public static getNativeWrapper(chainId: ChainId): Token {
+    return Token.NATIVE_CURRENCY_WRAPPER[chainId]
   }
 }
 
